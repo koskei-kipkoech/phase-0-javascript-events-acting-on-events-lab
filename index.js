6f067043bcc973761fdd68172ed80b9b6b1c68fb
Patrick Kipkoech <patrickwayy@gmail.com>
2024-09-23 11:17:46 +0300

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+const dodger = document.getElementById("dodger")
+function moveDodgerLeft(){
+    const left = parseInt(dodger.style.left, 10)
+    if (left>0){
+        dodger.style.left = `${left-1}px`;
+    }
+}
+function moveDodgerRight(){
+    const left = parseInt(dodger.style.left, 10);
+    if (left<360){
+        dodger.style.left = `${left + 1}px`
+    }
+}
