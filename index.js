722003ab0e741516e7b91b31db5660f6306ab186
koskei-kipkoech <80827464+koskei-kipkoech@users.noreply.github.com>
2024-10-05 01:12:25 +0300

    Updates on index.js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 // Your code here
-const dodger = document.getElementById("dodger")
-function moveDodgerLeft(){
-    const left = parseInt(dodger.style.left, 10)
-    if (left>0){
-        dodger.style.left = `${left-1}px`;
+const dodger = document.getElementById("dodger");
+// dodger.style.backgroundColor = '#ff69B4';
+// dodger.style.bottom = '0px';
+// dodger.style.left = '0px'
+// document.addEventListener("keydown", function (event) {
+//     console.log(event);
+// });
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+        if (left > 0) {
+            dodger.style.left = `${left - 1}px`;
+        }    
     }
-}
-function moveDodgerRight(){
-    const left = parseInt(dodger.style.left, 10);
-    if (left<360){
-        dodger.style.left = `${left + 1}px`
+    else if(event.key === "ArrowRight"){
+        const leftNumbers = dodger.style.left.replace("px","");
+        const left = parseInt(leftNumbers,10);
+        const maxLeft = 360;
+        if (left < maxLeft) {
+            dodger.style.left = `${left + 1}px`;
+        } 
     }
-}
+});
+
+
